4a8ca9d715c6cfaa615412a1a6f36117097aaaa1
Renan Adson <renan0409adson@gmail.com>
2026-08-17 08:17:20 -0300

    Add files via upload

--- Lab_Vitor/Labs/Lab_01/tb_mux_4.sv
+++ Lab_Vitor/Labs/Lab_01/tb_mux_4.sv
@@ -1,15 +1,25 @@
 `timescale 1ns/1ps
 
 module tb_mux_4;
-	logic [2:0]count;
-	logic muxOut;
+  logic [31:0] a, b, c, d;
+  logic [1:0]  sel;
+  logic [31:0] muxOut;
 
-	mux dut(.f(muxOut), .a(count[2]), .b(count[1]), .sel(count[0]));
+  mux_4 dut(.f(muxOut), .a(a), .b(b), .c(c), .d(d), .sel(sel));
 
-	initial begin
-		$monitor($time,"a = %b | b = %b | sel = %b | muxOut = %b", count[2], count[1], count[0], muxOut);
-		for(count = 0; count != 3'b111; count++) #10;     
-		#10 $stop;
-	end
+  initial begin
+    $monitor("%0t: sel = %b | a = %h | b = %h | c = %h | d = %h | muxOut = %h",
+              $time, sel, a, b, c, d, muxOut);
+
+    a = 32'hAAAA_AAAA;
+    b = 32'hBBBB_BBBB;
+    c = 32'hCCCC_CCCC;
+    d = 32'hDDDD_DDDD;
+
+    for (sel = 2'b00; sel != 2'b11; sel++) #10;
+    #10 sel = 2'b11; #10;
+
+    $stop;
+  end
 
 endmodule: tb_mux_4
