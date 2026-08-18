bafa73918a53dacb2c67e14f31a8de8588543f63
MiguelT-R-M <miguelhrs123@gmail.com>
2026-08-18 11:35:07 -0300

    LAB1

--- Lab_Vitor/Labs/Lab_01/mux_4.sv
+++ Lab_Vitor/Labs/Lab_01/mux_4.sv
@@ -1,21 +1,14 @@
-module mux_4
-  #(parameter WIDTH = 32)
-  (
-    output logic [WIDTH-1:0] f,
-    input  logic [WIDTH-1:0] a, b, c, d,
-    input  logic [1:0]       sel
-  );
-
-  logic [WIDTH-1:0] ab, cd;
-
-  genvar i;
-  generate
-    for (i = 0; i < WIDTH; i++) begin : bit_slice
-      mux m0(.f(ab[i]), .a(a[i]), .b(b[i]), .sel(sel[0]));
-      mux m1(.f(cd[i]), .a(c[i]), .b(d[i]), .sel(sel[0]));
-
-      mux m2(.f(f[i]),  .a(ab[i]), .b(cd[i]), .sel(sel[1]));
+module mux_4(
+	output logic [31:0] f,
+    input  logic [31:0] a, b, c, d,
+    input  logic [1:0] sel
+);
+    always_comb begin
+        case(sel)
+            2'b00: f = a;
+            2'b01: f = b;
+            2'b10: f = c;
+            2'b11: f = d;
+        endcase
     end
-  endgenerate
-
 endmodule
--- Lab_Vitor/Labs/Lab_01/tb_mux_4.sv
+++ Lab_Vitor/Labs/Lab_01/tb_mux_4.sv
@@ -1,25 +1,30 @@
 `timescale 1ns/1ps
 
 module tb_mux_4;
-  logic [31:0] a, b, c, d;
-  logic [1:0]  sel;
-  logic [31:0] muxOut;
+	logic [31:0] a, b, c, d;
+	logic [1:0]  sel;
+	logic [31:0] muxOut;
+	logic [2:0]count;
 
-  mux_4 dut(.f(muxOut), .a(a), .b(b), .c(c), .d(d), .sel(sel));
+	mux_4 dut(.f(muxOut), .a(a), .b(b), .c(c), .d(d), .sel(count[1:0]));
 
-  initial begin
-    $monitor("%0t: sel = %b | a = %h | b = %h | c = %h | d = %h | muxOut = %h",
-              $time, sel, a, b, c, d, muxOut);
+	initial begin
+		a = 32'b00000111111111100000000001111000;
+		b = 32'b11110000000000000000000011111111;
+		c = 32'b00011110000000001111111111111111;
+		d = 32'b00000000111111111111111111111111;
 
-    a = 32'hAAAA_AAAA;
-    b = 32'hBBBB_BBBB;
-    c = 32'hCCCC_CCCC;
-    d = 32'hDDDD_DDDD;
+		$monitor("%0t: sel = %b | a = %h | b = %h | c = %h | d = %h | muxOut = %h", $time, count[1:0], a, b, c, d, muxOut);
 
-    for (sel = 2'b00; sel != 2'b11; sel++) #10;
-    #10 sel = 2'b11; #10;
+		for(count = 0; count != 3'b111; count++) begin
+			a += 32'b10101;
+			b += 32'b1010101;
+			c += 32'b10101010101;
+			d += 32'b101010101010101;
+			#10;
+		end
 
-    $stop;
-  end
+		#10 $stop;
+	end
 
 endmodule: tb_mux_4
