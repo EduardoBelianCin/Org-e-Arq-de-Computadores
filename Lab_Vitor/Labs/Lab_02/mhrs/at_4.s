87542cba1593a3b3972c4ec12904a159ba030d79
MiguelT-R-M <miguelhrs123@gmail.com>
2026-08-22 20:38:02 -0300

    At 4-5 lab2

---
+++ Lab_Vitor/Labs/Lab_02/mhrs/at_4.s
@@ -0,0 +1,16 @@
+lw x28, point1 # Init reg pro ponteiro
+
+loop1:
+    lb x10, 0(x28) # Load byte no char atual da string
+    beq x10, x0, end1 # case pra acabar o while loo p
+    sb x10, 1024(x0) # Save byte na memória do vídeo do CompSim
+    addi x28, x28, 1 # Avança o ponteiro em 1 char
+    jal x1, loop1 # volta pro começo do loop
+end1:
+halt
+
+point1: .word str1 # Pointeiro pra strin g
+str1: .string "Hello World"
+
+
+
---
+++ Lab_Vitor/Labs/Lab_02/mhrs/at_5.s
@@ -0,0 +1,13 @@
+addi x28, x0, 42 # Valor em decimal do char '*'
+
+loop1:
+    lb x10, 1025(x0) # Load byte no char atual do tecladp
+    beq x10, x0, end1 # case pra acabar o while loop quando a frase acaba
+	beq x10, x28, end1 # case pra acabar quando o char for '*'
+    sb x10, 1024(x0) # Save byte na memória do vídeo do CompSim
+    jal x1, loop1 # volta pro começo do loop
+end1:
+halt
+
+
+	
