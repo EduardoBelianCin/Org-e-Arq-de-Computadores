f7025c0991bd97bf5e0c20d8bf97b8122e39b9eb
MiguelT-R-M <miguelhrs123@gmail.com>
2026-09-08 12:02:54 -0300

    Multiplicadores iniciais

---
+++ Lab_Vitor/Labs/Lab_04/multiplicador_refinado/alu_32.sv
@@ -0,0 +1,14 @@
+// alu_64.sv
+// ALU de 64 bits para a unidade de multiplicacao
+// Baseado na Figura 3.3 - Patterson & Hennessy, Computer Organization and Design
+//
+// Para o algoritmo de multiplicacao, apenas a operacao de soma e necessaria.
+
+module alu_32 (
+    input  logic [31:0] a,
+    input  logic [31:0] b,
+    output logic [31:0] sum
+);
+    assign sum = a + b;
+
+endmodule
